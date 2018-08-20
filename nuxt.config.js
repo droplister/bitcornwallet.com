44bb8ccc20a2566e1be8123b8438dda510382d44
Dan Anderson <anderson@familymedia.org>
2018-08-20 13:44:39 -0400

    Bootstrap Vue for Nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,12 @@ module.exports = {
   */
   loading: { color: '#3B8070' },
   /*
+  ** Loaded modules
+  */
+  modules: [
+    'bootstrap-vue/nuxt',
+  ],
+  /*
   ** Build configuration
   */
   build: {
